cb1e5e45fc60e22ba5f9b2d27776acd60bb1eb07
H21juliee <hectorblanc92@gmail.com>
2026-05-12 16:40:27 -0400

    Fix: Explicitly call global \fake() helper in StudentFactory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -12,15 +12,15 @@ class StudentFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'cedula' => 'V-' . fake()->unique()->numberBetween(10000000, 35000000),
-            'birth_date' => fake()->date('Y-m-d', '-10 years'),
-            'gender' => fake()->randomElement(['M', 'F']),
-            'address' => fake()->address(),
-            'guardian_name' => fake()->name(),
-            'guardian_phone' => fake()->phoneNumber(),
-            'guardian_email' => fake()->safeEmail(),
+            'first_name' => \fake()->firstName(),
+            'last_name' => \fake()->lastName(),
+            'cedula' => 'V-' . \fake()->unique()->numberBetween(10000000, 35000000),
+            'birth_date' => \fake()->date('Y-m-d', '-10 years'),
+            'gender' => \fake()->randomElement(['M', 'F']),
+            'address' => \fake()->address(),
+            'guardian_name' => \fake()->name(),
+            'guardian_phone' => \fake()->phoneNumber(),
+            'guardian_email' => \fake()->safeEmail(),
             'is_active' => true,
         ];
     }
